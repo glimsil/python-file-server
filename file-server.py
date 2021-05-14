acbcad7f6042c98bd1318136d160f32e1a90ca6f
Gustavo de Lima <gusdlim@gmail.com>
2021-05-14 09:35:06 -0300

    aggregated

--- file-server.py
+++ file-server.py
@@ -16,7 +16,17 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
  
- 
+# Check the file name, by date
+#  consider DATE_HOUR.TXT
+def is_txt_from_date(filename, date):
+    is_txt = '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() == 'txt'
+    if is_txt:
+        true_name = filename.rsplit('.', 1)[0].lower()
+        is_date = '_' in true_name and true_name.split('_')[0].lower() == date.lower()
+        return is_date
+    return False
+
 @app.route('/', methods=['GET','POST'])
 def index():
  
@@ -55,7 +65,36 @@ def index():
     ''' % file_list
  
     return return_html
- 
+
+# Aggregate results by date
+# Consider DATE_HOUR.TXT
+@app.route('/agg/<date>', methods=['GET'])
+def aggregated(date):
+    column_names=""
+    aggregated = ""
+    first = True
+    for f in os.listdir(app.config['UPLOAD_FOLDER']):
+        if is_txt_from_date(f, date):
+            print(f)
+            content = open(app.config['UPLOAD_FOLDER'] + f, "r")
+            aggregated_line = ""
+            lines = list(content)
+            if first:
+                for line in lines:
+                    column_names = column_names + line.split(':')[0] + ';'
+                first = False
+                column_names = column_names[:-1] + "<br>"
+            for line in lines:
+                if(':' in line):
+                    aggregated_line = aggregated_line + line.split(':')[1].strip() + ';'
+                else:
+                    aggregated_line = aggregated_line + '--;'
+            aggregated_line = aggregated_line[:-1] + "<br>"
+            print(aggregated_line)
+            aggregated = aggregated + aggregated_line
+    
+    aggregated = column_names + aggregated
+    return aggregated
  
 if __name__ == '__main__':
     app.run(debug=True)
